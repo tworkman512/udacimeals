f20b2eb4502d36c58a7a49e9072461a50dbe153e
tom-workman <tom.workman@us.ibm.com>
2017-08-14 16:36:25 -0500

    Add submitFood method

--- src/index.js
+++ src/index.js
@@ -11,6 +11,5 @@ const store = createStore(
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
