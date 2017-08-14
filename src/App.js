3db6132527dc478b398c0a5f4201d1e0d77f0f2f
tom-workman <tom.workman@us.ibm.com>
2017-08-14 07:02:54 -0500

    Add utils and edit app.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Hello World
       </div>
-    );
+    )
   }
 }
 
