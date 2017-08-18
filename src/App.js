cbc5f6099866f4d3b32eafa6427a0c9bbdff4847
tom-workman <tom.workman@us.ibm.com>
2017-08-18 15:53:41 -0500

    Add react-redux bindings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from './actions'
 
 class App extends Component {
 
@@ -31,4 +32,11 @@ function mapStateToProps(calendar) {
 	}
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
