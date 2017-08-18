700493ed60eb88e4b294ff37d6773e15ed0fa776
tom-workman <tom.workman@us.ibm.com>
2017-08-18 14:58:57 -0500

    Add & hook up Provider

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(
 	reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, document.getElementById('root'));
 registerServiceWorker();
